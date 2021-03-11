43a3f67e0e9d3d3ffaad7bd850d3b5e8212ac649
Ganesh rao <raoganesh80@gmail.com>
2021-03-11 13:32:33 +0530

    Update views.py

--- bulkWhatAppMsg/views.py
+++ bulkWhatAppMsg/views.py
@@ -59,7 +59,7 @@ def index(request):
                 
                 # link chrome driver
                 chrome_options = webdriver.ChromeOptions()
-                chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+                # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
                 chrome_options.add_argument("--headless")
                 chrome_options.add_argument("--disable-dev-shm-usage")
                 chrome_options.add_argument("--no-sandbox")
