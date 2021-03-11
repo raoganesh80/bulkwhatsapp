4f9ac3030abd0637471622d44f8d6af208be23b6
Ganesh rao <raoganesh80@gmail.com>
2021-03-11 13:19:04 +0530

    web driver problem

--- bulkWhatAppMsg/views.py
+++ bulkWhatAppMsg/views.py
@@ -58,7 +58,14 @@ def index(request):
                 msg=msg.replace('<tab>','%09')
                 
                 # link chrome driver
-                driver = webdriver.Chrome()
+                chrome_options = webdriver.ChromeOptions()
+                chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+                chrome_options.add_argument("--headless")
+                chrome_options.add_argument("--disable-dev-shm-usage")
+                chrome_options.add_argument("--no-sandbox")
+                driver = webdriver.Chrome(executable_path=os.environ.get(
+                    "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
                 # open web.whatsapp.com for QR Code scaning.
                 driver.get("https://web.whatsapp.com/")
                 time.sleep(15)
