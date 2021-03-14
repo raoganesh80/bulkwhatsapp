baa2e1ce460764e75ce961a835b794d3fdd2686d
Ganesh rao <raoganesh80@gmail.com>
2021-03-14 20:59:22 +0530

    Update views.py

--- bulkWhatAppMsg/views.py
+++ bulkWhatAppMsg/views.py
@@ -81,12 +81,8 @@ def index(request):
                 msg = msg.replace('<tab>', '%09')
 
                 # link chrome driver to server envirorment.
-                chrome_options = webdriver.ChromeOptions()
-                chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-                chrome_options.add_argument("--headless")
-                chrome_options.add_argument("--disable-dev-shm-usage")
-                chrome_options.add_argument("--no-sandbox")
-                driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+                driver = webdriver.Chrome(
+                    executable_path=os.environ.get("GECKODRIVER_PATH"))
 
                 # driver = webdriver.Chrome()  # this code will work only local server
 
